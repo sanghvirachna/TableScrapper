ec1a7565d6f5e44a372b0a40aac2e65be2681db1
sanghvirachna <rachnasanghvi123@gmail.com>
2024-03-07 18:25:12 +0530

    added funtionality to download csv from one than more table

--- content.js
+++ content.js
@@ -1,17 +1,21 @@
-
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action === "download") {
-      var table = document.querySelector('table'); 
-      var rows = Array.from(table.rows); 
-      var csvContent = rows.map(row => Array.from(row.cells).map(cell => cell.innerText).join(',')).join('\n');
-  
+  if (request.action === "download") {
+      var tables = document.querySelectorAll('table'); 
+      var csvContent = '';
+
+      tables.forEach(function(table) {
+          var rows = Array.from(table.rows); 
+          csvContent += rows.map(row => Array.from(row.cells).map(cell => cell.innerText).join(',')).join('\n');
+          csvContent += '\n'; // Add a newline to separate different tables
+      });
+
       var blob = new Blob([csvContent], {type: 'text/csv'});
       var url = URL.createObjectURL(blob);
-  
+
       // Create a link and click it to start the download
       var link = document.createElement('a');
       link.href = url;
-      link.download = 'table.csv';
+      link.download = 'tables.csv';
       link.click();
-    }
-  });
+  }
+});
