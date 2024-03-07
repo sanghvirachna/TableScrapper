d76dc5f4da5ef2e5fc38897b3417875901a553a7
sanghvirachna <rachnasanghvi123@gmail.com>
2024-03-07 18:42:25 +0530

    Formatted CSV for multiple table , added styles

--- content.js
+++ content.js
@@ -1,14 +1,21 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action === "download") {
-      var tables = document.querySelectorAll('table'); 
-      var csvContent = '';
+      var tables = document.querySelectorAll('table'); // Select all tables
+      var csvContent = ""; // Initialize CSV content
 
       tables.forEach(function(table) {
+          // Add an empty line between tables
+          csvContent += "\n\n";
+
+          // Extract data from each table
           var rows = Array.from(table.rows); 
-          csvContent += rows.map(row => Array.from(row.cells).map(cell => cell.innerText).join(',')).join('\n');
-          csvContent += '\n'; // Add a newline to separate different tables
+          rows.forEach(function(row) {
+              var rowData = Array.from(row.cells).map(cell => cell.innerText).join(',');
+              csvContent += rowData + '\n'; // Append row data to CSV content
+          });
       });
 
+      // Create a blob with the CSV content
       var blob = new Blob([csvContent], {type: 'text/csv'});
       var url = URL.createObjectURL(blob);
 
